47aef1befe20e8643e7f82b87a02a97a6dee53a7
belvoixnicolas <belvoixnicolas1997@gmail.com>
2018-10-23 15:40:36 +0200

    profil update

--- js/mailprofil.js
+++ js/mailprofil.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function(){
     $('#form_mail').submit( function () { return false; });
-    $('#form_mail').hide();
+    $('#form_mail, #form_mail label[for=join]').hide();
 
     $('#mail span:nth-child(1)').click(function () {
         $('#form_mail').animate({height: 'toggle'}, 'slow');
