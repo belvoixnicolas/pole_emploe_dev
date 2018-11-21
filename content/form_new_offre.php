027eb5fe2a6a7d556b36de42347a4bb5b9b2ed89
belvoixnicolas <belvoixnicolas1997@gmail.com>
2018-11-21 17:30:13 +0100

    t

--- content/form_new_offre.php
+++ content/form_new_offre.php
@@ -1,8 +1,17 @@
-<form action="<?php echo $_SERVER['REQUEST_URI']?>" method="post" enctype="multipart/form-data">
-    <input type="text" name="titre" placeholder="Titre">
-    <textarea name="descript"></textarea>
-    <input type="number" name="temp" placeholder="temp">
-    <input type="number" name="prix" placeholder="prix">
-    
-    <input type="submit" value="Poster">
-</form>
+<section id="formprojet">
+    <form action="" method="post" enctype="multipart/form-data">
+        <input type="text" name="titre" placeholder="Titre">
+        <textarea name="descript" placeholder="Description"></textarea>
+        <input type="number" name="temp" min="1" placeholder="temp">
+        <select name="temp_mesure">
+            <option value="jour">Jour</option>
+            <option value="semaine">Semaine</option>
+            <option value="mois">Mois</option>
+            <option value="anner">Anner</option>
+        </select>
+        
+        <button id="envoyer" type="button">Poster</button>
+    </form>
+</section>
+
+<script src="./js/ajax/ajout_projet.js"></script>
---
+++ emploie_cree.php
@@ -0,0 +1,39 @@
+<?php 
+    require_once('./php/user.php');
+    session_start();
+
+    require_once('./php/connect.php');
+    require_once('./php/secu_acces.php');
+    require_once('./php/notif_error.php');
+
+    autoris_acces(2);
+
+    $_SESSION['iduser'] = $_SESSION['user']->get('id');
+?>
+<!DOCTYPE HTML>
+  <html lang="fr">
+    <head>
+        <meta name="theme-color" content="white">
+        <?php include './content/header_base.html' ?>
+        <title>Emploie</title>
+    </head>
+    <body id="emploie_cree">
+        <?php echo notif_error($errors); ?>
+
+        <!-- nav --> 
+            <?php include './content/nav.php' ?>
+        <!-- nav -->
+
+        <!-- SOUS-NAV -->
+            <?php include './content/sous_nav_emploie.php' ?>
+        <!-- SOUS-NAV -->
+
+        <main>
+            <?php include './content/form_new_offre.php' ?>
+        </main>
+
+        <!-- FOOTER -->
+        <?php include './content/footer.html' ?>
+        <!-- FOOTER -->
+    </body>
+  </html>
